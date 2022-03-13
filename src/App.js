17f69e3542f74425d9e92ac34874eac07c1bc542
Shams Sikder <shams@Shamss-Air.attlocal.net>
2022-03-12 23:24:03 -0500

    Fixed more ESlinter errors

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-  
-function App(){
+
+function App() {
 
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    fetch("/comments").then(response =>
+    fetch('/comments').then(response => 
       response.json().then(data => {
         setComments(data);
       })
-    )
-  }, [])
+    );
+  }, []);
 
-  function handleRemove(id){
-    fetch('/comment/'+id, {
+  function handleRemove(id) {
+    fetch('/comment/' + id, {
       method: 'DELETE',
       header: {
-         'Accept' : 'application/json',
-         'Content-Type' : 'application/json',
-        }
-      })
-      setComments(comments.filter(comment => comment.id !== id));
-    }
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    setComments(comments.filter(comment => comment.id !== id));
+  }
 
   function handleRating() {
-    const [rating, setRating] = useState("");
+    const [rating, setRating] = useState('');
   }
 
   return (
-    <div className="App">
+    <div className='main'>
       <table>
         <tr>
           <th>Movie ID: </th>
@@ -40,7 +40,7 @@ function App(){
           return (
             <tr key={key}>
               <td>{val.movie}</td>
-              <td><input type="text" value={val.rating}/></td>
+              <td><input type='text' value={val.rating}/> </td>
               <td>{val.review}</td>
               <td><button onClick={() => handleRemove(val.id)}>Delete!</button></td>
             </tr>
